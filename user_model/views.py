f890d9c1eb2bffc006c165e91d536cf8c977d023
fuadmmnf <bsse0920@iit.du.ac.bd>
2019-06-04 16:33:23 +0600

    basic model user form working

---
+++ user_model/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .forms import NewUserForm
+# Create your views here.
+
+def index(request):
+    return render(request, "user_model/index.html")
+
+def users(request):
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("ERROR FORM INVALID")
+
+    return render(request, 'user_model/users.html', {'form': form})
